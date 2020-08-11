42de017a97134ab09c3c614fbe744e61c681a42d
wendyorengo <wenndyorengo@gmail.com>
2020-08-11 19:44:23 +0300

    add paragraph

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
-    $(".img").click(function(){
-        $(".para").toggle();
- });
+// $(document).ready(function(){
+//     $(".img").click(function(){
+//         $(".para").toggle();
+//  });
 
-})
+// })
